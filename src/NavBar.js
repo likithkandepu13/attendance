ecddef217703b37c381be6cfcc2d44f1d0a276ba
Likith Kandepu <withlikith@gmail.com>
2024-05-17 10:46:22 +0530

    changes to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import Calc1 from './Calc1';
 import Calc2 from './Calc2';
 import Calc3 from './calc3';
@@ -11,8 +11,8 @@ const NavBar = () => {
         <div className="navbar2">
             <ResponsiveAppBar />
             <Routes>
-            <Route path="/" element={<Navigate to="/home" />} /> {/* Redirect from "/" to "/home" */}
-                <Route path="/attendance" element={<Navigate to="/home" />} /> {/* Redirect from "/" to "/home" */}
+            <Route path="/" element={<Home/>} /> {/* Redirect from "/" to "/home" */}
+                <Route path="/attendance" element={<Home/>} /> {/* Redirect from "/" to "/home" */}
                 <Route path="/home" element={<Home />} />
                 <Route path="/total" element={<Calc1 />} />
                 <Route path="/calbyltps" element={<Calc2 />} />
